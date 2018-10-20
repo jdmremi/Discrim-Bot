a04ba0a7d12084ff5e0f2846722f5468b999bbf4
Mike <29011818+mikeo@users.noreply.github.com>
2018-10-20 19:29:34 +0100

    fixes
    longer delay between any attempts due to discord being harsh

--- main.js
+++ main.js
@@ -1,35 +1,42 @@
-const myDiscriminator = ["0000", "0001", "0002", "0003", "0004", "0005", "0006", "0007", "0008", "0009", "1337"];
-const bot = new(require("discord.js")).Client({fetchAllMembers: true});
+const myDiscriminator = ['0001', '0002'];
+const bot = new(require("discord.js")).Client({
+    fetchAllMembers: true
+});
 
-const password = "Your password here";
-const token = "Your token here";
+const password = "*password*";
+const token = "*token*";
 
 bot.login(token);
 
 function changeDiscriminator() {
-  if (myDiscriminator.includes(bot.user.discriminator.toString()))
-    return console.log("Discriminator Loaded: " + bot.user.discriminator);
-  try {
-    const us = bot.users.find(u => u.discriminator === bot.user.discriminator && u.username !== bot.user.username && !u.bot).username;
-    console.log(Date.now(), "Username Loaded: " + us);
-    bot.user.setUsername(us, password).then((u) => {
-      console.log(Date.now(), "Username: " + u.username, "Discriminator: " + u.discriminator);
-      setTimeout(changeDiscriminator, 8640 * 10000);
-    }).catch((err) => {
-      console.log(Date.now(), "An error occurred. Trying again in sixty (60) seconds.");
-      setTimeout(changeDiscriminator, 60 * 1e3);
-    });
-  } catch(e) {
-    console.log(Date.now(), `[${e}] Trying again in 30 seconds.`);
-    setTimeout(changeDiscriminator, 30 * 1e3);
-  }
+    try {
+        if (myDiscriminator.includes(bot.user.discriminator.toString()))
+            return console.log("Discriminator Loaded: " + bot.user.discriminator);
+        try {
+            const us = bot.users.find(u => u.discriminator === bot.user.discriminator && u.username !== bot.user.username && !u.bot).username;
+            console.log(new Date(), "Username Loaded: " + us);
+            bot.user.setUsername(us, password).then((u) => {
+                console.log(new Date(), "Username: " + u.username, "Discriminator: " + u.discriminator);
+                setTimeout(changeDiscriminator, 864 * 2e5);
+            }).catch((err) => {
+                console.log(new Date(), "An error occurred. Trying again in a day");
+                setTimeout(changeDiscriminator, 864 * 2e5);
+            });
+        } catch (e) {
+            console.log(new Date(), `[${e}] Trying again in a day.`);
+            setTimeout(changeDiscriminator, 864 * 2e5);
+        }
+    } catch (err){
+		console.log(err.message);
+	}
 }
 
+
 bot.once("ready", () => {
-  console.log(Date.now(), "Started with " + bot.users.size + " users.");
-  changeDiscriminator();
-  if(myDiscriminator.includes(bot.user.discriminator)) {
-      console.log(`I successfully got the discrim ${bot.user.discriminator}!`) 
-      process.exit();
-  }
+    console.log(new Date(), "Started with " + bot.users.size + " users.");
+    changeDiscriminator();
+    if (myDiscriminator.includes(bot.user.discriminator)) {
+        console.log(`I successfully got the discrim ${bot.user.discriminator}!`)
+        process.exit();
+    }
 });
